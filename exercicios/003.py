8effdaba997ee966906dd2e697abeffe62eb5eab
Beatriz Sara <bea.sara.santos@outlook.com>
2023-10-16 14:32:35 -0300

    Exerxicio 003

---
+++ exercicios/003.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+primeiro = int(input("Informe o primeiro número: "))
+segundo = int(input("Informe o segundo número: "))
+print("A soma dos dois números é: ", primeiro + segundo)
