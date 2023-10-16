890d666be6b178ad7c242cf41c518f46ad8c493b
Beatriz Sara <bea.sara.santos@outlook.com>
2023-10-16 14:57:57 -0300

    Exercicio 004

---
+++ exercicios/004.py
@@ -0,0 +1,11 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+bim01 = float(input("Informe a nota do 1º Bimestre: "))
+bim02 = float(input("Informe a nota do 2º Bimestre: "))
+bim03 = float(input("Informe a nota do 3º Bimestre: "))
+bim04 = float(input("Informe a nota do 4º Bimestre: "))
+
+media = (bim01+bim02+bim03+bim04) / 4
+
+print("A média Bimestral é: ", media)
