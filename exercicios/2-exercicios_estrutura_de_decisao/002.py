636f71d1389720a41371ee6c4fb4b72a78be0343
Beatriz Sara <bea.sara.santos@outlook.com>
2023-10-22 12:05:51 -0300

    exercicio estrutura decisao 002

---
+++ exercicios/2-exercicios_estrutura_de_decisao/002.py
@@ -0,0 +1,9 @@
+""" 
+Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+"""
+numero = int(input("Informe um numero positivo ou negativo: "))
+
+if numero > 0:
+    print("O número é positivo!")
+else:
+    print("O número é negativo!")
