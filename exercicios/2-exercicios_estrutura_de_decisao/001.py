d2369d97a202d3a30fcf21781fcfa3b3445e27bb
Beatriz Sara <bea.sara.santos@outlook.com>
2023-10-22 11:52:07 -0300

    exercicio estrutura decisao 001

---
+++ exercicios/2-exercicios_estrutura_de_decisao/001.py
@@ -0,0 +1,10 @@
+""" 
+Faça um Programa que peça dois números e imprima o maior deles.
+"""
+primeiro_numero = int(input("Informe um número: "))
+segundo_numero = int(input("Informe um segundo número: "))
+
+if primeiro_numero > segundo_numero:
+    print(" O primeiro número é maior que o segundo!")
+else:
+    print(" O segundo número é maior que o primeiro!")
