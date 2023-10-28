3ff070a6bc5c72ac28053cbf1932a04b37d3efd4
Beatriz Sara <bea.sara.santos@outlook.com>
2023-10-28 20:20:43 -0300

    Iniciado exercicio estrutura decisao 015

---
+++ exercicios/2-exercicios_estrutura_de_decisao/015.py
@@ -0,0 +1,27 @@
+""" 
+Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. 
+Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+Dicas:
+Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+Triângulo Equilátero: três lados iguais;
+Triângulo Isósceles: quaisquer dois lados iguais;
+Triângulo Escaleno: três lados diferentes;
+"""
+
+## Pede os 3 lados. Faz o calculo da primeira dica pra dizer se com os lados informados é possivel formar um triangulo, se sim calcular se ele é equilatero, isosceles ou escaleno
+
+lado1 = float(input("informe o valor do lado de um triângulo: "))
+lado2 = float(input("informe o valor do segundo lado de um triângulo: "))
+lado3 = float(input("informe o valor do terceiro lado de um triângulo: "))
+
+if lado1 + lado2 > lado3
+    
+    if lado1 == lado2 or lado2 == lado3:
+        print("Triângulo é Equilátero!")
+    elif 
+        print("Triângulo é Isósceles!")
+    elif:
+        print("Triângulo é Escaleno!")
+        
+    if () or ():
+    
