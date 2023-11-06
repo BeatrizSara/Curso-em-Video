c85481a6aec4a036a332e2022ff8b64fe671674f
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-05 23:55:41 -0300

    exercicio estrutura decisao 024

---
+++ exercicios/2-exercicios_estrutura_de_decisao/024.py
@@ -0,0 +1,51 @@
+""" 
+Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. 
+O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+a) par ou ímpar;
+b) positivo ou negativo;
+c) inteiro ou decimal.
+"""
+import sys
+
+num1 = float(input("Informe um número: "))
+num2 = float(input("Informe um segundo número: "))
+operacao = input("Qual operação deseja realizar? (+, -, * ou /)")
+
+match operacao:
+    
+    case "+":
+        resultado = num1 + num2
+    case "-":
+        resultado = num1 - num2
+    case "*":
+        resultado = num1 * num2
+    case "/":
+        resultado = num1 / num2
+    case _:
+        print("Operação inválida.")
+        sys.exit()
+
+
+print(f"O resultado da operação de {num1} {operacao} {num2} é {resultado}")
+        
+restoDivisao = resultado % 2
+
+if restoDivisao == 0:
+    print("O número é par!")
+else:
+    print("O número é ímpar!")
+
+arredondado = round(resultado)
+
+if arredondado == resultado:
+    print("O número é inteiro! ")
+
+else:
+    print("O número é decimal! ")
+    
+if resultado > 0:
+    print("O número é positivo.")
+elif resultado < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
