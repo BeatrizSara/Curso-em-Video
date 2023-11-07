3ecb37548dd39e0c4c59c8579095255c11596f93
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-07 16:13:01 -0300

    editado exercicio estrutura decisao 026

--- exercicios/2-exercicios_estrutura_de_decisao/026.py
+++ exercicios/2-exercicios_estrutura_de_decisao/026.py
@@ -9,7 +9,6 @@ acima de 20 litros, desconto de 6% por litro
 Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), 
 calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 """
-# .upper() letra maiuscula para A e G
 print("Informe A para Álcool ou G para Gasolina, nas informações a seguir: ")
 
 tipo_combustivel = input("Qual o tipo de combustível? ").upper()
