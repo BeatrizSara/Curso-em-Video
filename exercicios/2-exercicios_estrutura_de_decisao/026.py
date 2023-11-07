997c124e66a3454d0a596c4bc191f69f7c130973
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-07 16:12:24 -0300

    exercicio estrutura decisão 026

---
+++ exercicios/2-exercicios_estrutura_de_decisao/026.py
@@ -0,0 +1,41 @@
+""" 
+Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+Álcool:
+até 20 litros, desconto de 3% por litro
+acima de 20 litros, desconto de 5% por litro
+Gasolina:
+até 20 litros, desconto de 4% por litro
+acima de 20 litros, desconto de 6% por litro 
+Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), 
+calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
+"""
+# .upper() letra maiuscula para A e G
+print("Informe A para Álcool ou G para Gasolina, nas informações a seguir: ")
+
+tipo_combustivel = input("Qual o tipo de combustível? ").upper()
+litros_vendidos = float(input("Quantos litros ? "))
+
+preco_gasolina = 2.50
+preco_alcool = 1.90
+desconto = 0.0
+valor_total = 0.0
+
+if tipo_combustivel == "A":
+    
+    if litros_vendidos <= 20:
+        desconto = litros_vendidos * 0.03
+    else:
+        desconto = litros_vendidos * 0.05
+
+    valor_total = (litros_vendidos * preco_alcool) - desconto
+
+if tipo_combustivel == "G":
+    
+    if litros_vendidos <= 20:
+        desconto = litros_vendidos * 0.04
+    else:
+        desconto = litros_vendidos * 0.06
+    
+    valor_total = (litros_vendidos * preco_gasolina) - desconto        
+
+print(f" O valor total a ser pago é: R$ {valor_total:.2f}")
