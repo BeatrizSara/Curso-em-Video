e95c94596ea8a9b71bf38856f5fcaaac89fd1d50
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-10 19:19:44 -0300

    exercicio revisao 02

---
+++ exemplos_aprendizado/exercicio_02.py
@@ -0,0 +1,13 @@
+""" 
+Desafio 02
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+"""
+numero = float(input("Informe um número: "))
+
+dobro = numero * 2 
+triplo = numero * 3
+raizQ = numero ** (1/2)
+
+print(f"O dobro é {dobro}")
+print(f"O triplo é {triplo}")
+print(f"A raiz quadrada é {raizQ}")
