7c311960cf95f9a390c62eff876bc0292c855194
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-10 19:17:59 -0300

    exercicio revisao 01

---
+++ exemplos_aprendizado/exercicio_01.py
@@ -0,0 +1,11 @@
+""" 
+Desafio 01
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+"""
+
+numeroInt = int(input("Informe um número inteiro :"))
+
+sucessor = numeroInt + 1
+antecessor = numeroInt - 1
+
+print(f"Seu sucessor é {sucessor}, e seu antecessor é {antecessor}")
