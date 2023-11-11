f7db10fc266325f2716e503da01a2a65c4c9eea5
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-10 23:45:35 -0300

    exercicio revisao 03

---
+++ exemplos_aprendizado/exercicio_03.py
@@ -0,0 +1,12 @@
+"""  
+Desafio 03
+Desenvolva um programa que desenvolva duas notas de um aluno. 
+Calcule e mostre sua média
+"""
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+
+
+mediaFinal = (nota1 + nota2) / 2
+
+print(f" A média final é : {mediaFinal:.2f}")
