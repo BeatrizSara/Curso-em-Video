3c505fc391786b07fe19ffdb6aa297583385229e
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-10 23:45:50 -0300

    exercicio revisao 05

---
+++ exemplos_aprendizado/exercicio_05.py
@@ -0,0 +1,31 @@
+"""
+Desafio 05
+Faça um programa que leia um número inteiro qualquer e mostre  na tela sua tabuada
+"""
+
+numeroInt = int(input("Informe um número inteiro qualquer: "))
+
+zero = numeroInt * 0
+um = numeroInt * 1
+dois = numeroInt * 2
+tres = numeroInt * 3 
+quatro = numeroInt * 4
+cinco = numeroInt * 5
+seis = numeroInt * 6
+sete = numeroInt * 7
+oito = numeroInt * 8
+nove = numeroInt * 9
+dez = numeroInt * 10
+
+print(f"A tabuada do numero {numeroInt} é: ")
+print(f"{numeroInt} x 0 = {zero}")
+print(f"{numeroInt} x 1 = {um}")
+print(f"{numeroInt} x 2 = {dois}")
+print(f"{numeroInt} x 3 = {tres}")
+print(f"{numeroInt} x 4 = {quatro}")
+print(f"{numeroInt} x 5 = {cinco}")
+print(f"{numeroInt} x 6 = {seis}")
+print(f"{numeroInt} x 7 = {sete}")
+print(f"{numeroInt} x 8 = {oito}")
+print(f"{numeroInt} x 9 = {nove}")
+print(f"{numeroInt} x 10 = {dez}")
