fb5439c123fa48985abeda4a66edb2e7ad9aee7c
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-13 23:28:02 -0300

    iniciando estrutura repeticao 007

---
+++ exercicios/3-estrutura_de_repeticao/007.py
@@ -0,0 +1,9 @@
+""" 
+Faça um programa que leia 5 números e informe o maior número.
+"""
+n1 = int(input("Informe um numero:"))
+n2 = int(input("Informe um segundo numero:"))
+n3 = int(input("Informe um terceiro numero:"))
+n4 = int(input("Informe um quarto numero:"))
+n5 = int(input("Informe um quinto numero:"))
+
