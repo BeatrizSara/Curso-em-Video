64c5b20a049364eeed865d7057f3ceabff476f44
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-14 23:50:31 -0300

    finalizado exercicio estrutura repeticao 001

--- exercicios/3-estrutura_de_repeticao/001.py
+++ exercicios/3-estrutura_de_repeticao/001.py
@@ -2,3 +2,13 @@
 Faça um programa que peça uma nota, entre zero e dez. 
 Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 """
+while True:
+    nota = float(input("Informe uma nota entre zero a dez: "))
+
+    if 0 <= nota <= 10:
+        break
+
+    else:
+        print("Valor não válido. A nota deve ser entre zero e dez.")
+
+print(f"A nota informada foi: {nota}")
