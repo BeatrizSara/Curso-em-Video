4d21f8abe17e86dc0b5df483842d42bddf2896ce
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-20 19:31:47 -0300

    iniciando exercicio estrutura decisao 003

--- exercicios/3-estrutura_de_repeticao/003.py
+++ exercicios/3-estrutura_de_repeticao/003.py
@@ -6,3 +6,9 @@ Salário: maior que zero;
 Sexo: 'f' ou 'm';
 Estado Civil: 's', 'c', 'v', 'd';
 """
+while True:
+    nome = input("Informe o nome: ")
+    idade = int("Informe a idade: ")
+    salario = float(input("Informe o salário: R$"))
+    sexo = input("Informe o sexo, F- Feminino ou M - Masculino: ").upper()
+    estado_civil = input("Informe o estado civil, S - solteiro, C- casado, V - viúvo, D- Divorciado: ").upper()
