d3c57be58ccc3175dc78693be89efcabd036b1d4
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-20 19:22:38 -0300

    finalizado exercicio estrutura repeticao 002

--- exercicios/3-estrutura_de_repeticao/002.py
+++ exercicios/3-estrutura_de_repeticao/002.py
@@ -2,3 +2,14 @@
 Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
 mostrando uma mensagem de erro e voltando a pedir as informações.
 """
+while True:
+    
+    usuario = input("Informe o nome de usuário: ")
+    senha = input("Informe a senha: ")
+    
+    if senha == usuario:
+        print("Não é possível a senha ser igual ao nome de usuário! \nTente novamente.")
+    
+    else:
+        print("Cadastro realizado com sucesso!!")
+        break
