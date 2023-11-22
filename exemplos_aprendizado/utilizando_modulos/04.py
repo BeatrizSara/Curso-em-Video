61df31bc2e1d28d9c417e26bde7fadb167cbbc64
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-22 20:32:59 -0300

    alterado exercicio utilizando modulos 04

--- exemplos_aprendizado/utilizando_modulos/04.py
+++ exemplos_aprendizado/utilizando_modulos/04.py
@@ -3,7 +3,7 @@ Sorteando um item na lista
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 """
-import random 
+from random import choice
 
 n1 = input("Informe um nome:")
 n2 = input("Informe um segundo nome:")
@@ -12,6 +12,6 @@ n4 = input("Informe um quarto nome:")
 
 lista = [n1,n2,n3,n4]
 
-escolhido = random.choice(lista)
+escolhido = choice(lista)
 
 print(f"O aluno escolhido foi: {escolhido}")
