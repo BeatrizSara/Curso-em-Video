88fe29987ce52fc718308a3afc335507b829093f
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-22 19:35:45 -0300

    alterado exercicio utilizando modulos 02

--- exemplos_aprendizado/utilizando_modulos/02.py
+++ exemplos_aprendizado/utilizando_modulos/02.py
@@ -2,11 +2,11 @@
 Catetos e Hipotenusa 
 Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 """
-import math
+from math import hypot
 
 cateto_oposto = float(input("Informe o comprimento do cateto oposto: "))
 cateto_adjacente = float(input("Informe o comprimento do cateto adjacente: "))
 
-hipotenusa = math.hypot(cateto_oposto, cateto_adjacente)
+hipotenusa = hypot(cateto_oposto, cateto_adjacente)
 
 print(f"A hipotenusa vai medir {hipotenusa:.2f}")
