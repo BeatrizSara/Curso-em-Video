b5b6556ce76dbb3b49cd06639b6fb57c83be9860
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-26 22:35:38 -0300

    iniciando exercicio manipulador de textos 01

---
+++ exemplos_aprendizado/manipulando_texto/01.py
@@ -0,0 +1,7 @@
+""" 
+Analisador de textos
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+- O nome com todas as letras maiusculas e minusculas 
+- Quantas letras ao todo (sem considerar espaços)
+- Quantas letras tem o primeiro nome
+"""
