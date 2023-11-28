8231ff95989131e26d9fcea4a7214a19e68d6200
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-27 23:49:21 -0300

    finalizado exercicio estrutura repeticao 003

--- exercicios/3-estrutura_de_repeticao/003.py
+++ exercicios/3-estrutura_de_repeticao/003.py
@@ -6,9 +6,50 @@ Salário: maior que zero;
 Sexo: 'f' ou 'm';
 Estado Civil: 's', 'c', 'v', 'd';
 """
+
 while True:
     nome = input("Informe o nome: ")
-    idade = int("Informe a idade: ")
+    
+    if len(nome) > 3:
+        break
+    else: 
+        print("O nome deve possuir mais do que 3 caracteres")
+
+while True:
+    idade = int(input("Informe a idade: "))
+
+    if idade > 0 and idade < 150:
+        break
+    else:
+        print("Deve informar a idade entre 0 e 150.")
+
+while True:
     salario = float(input("Informe o salário: R$"))
+
+    if salario > 0:
+        break
+    else:
+        print("O salário deve ser maior que zero.")
+
+while True:
     sexo = input("Informe o sexo, F- Feminino ou M - Masculino: ").upper()
+
+    if sexo == "F" or sexo == "M":
+        break
+    else:
+        print("O sexo deve ser M para masculino ou F para Feminino.")
+        
+while True:
     estado_civil = input("Informe o estado civil, S - solteiro, C- casado, V - viúvo, D- Divorciado: ").upper()
+
+    match estado_civil:
+        case "S":
+            break
+        case "C":
+            break
+        case "V":
+            break
+        case "D":
+            break
+        case _:
+            print("Informe o estado civil solicitado.")
