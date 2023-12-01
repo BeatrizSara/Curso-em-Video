782e82a253b303a67f1f6725b107d6b011d2d67c
Beatriz Sara <bea.sara.santos@outlook.com>
2023-11-30 23:33:07 -0300

    finalizado exercicio manipuando textos 01

--- exemplos_aprendizado/manipulando_texto/01.py
+++ exemplos_aprendizado/manipulando_texto/01.py
@@ -5,3 +5,15 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 - Quantas letras ao todo (sem considerar espaços)
 - Quantas letras tem o primeiro nome
 """
+
+nome = str(input("Digite seu nome completo: ")).strip()
+
+print("Analisando seu nome... ")
+
+print(f"Seu nome em maiúsculas é {nome.upper()}")
+print(f"Seu nome em minusculas é {nome.lower()}")
+print(f"Seu nome possui {len(nome) - nome.count(' ')} letras")
+#print(f"Seu primero nome tem {nome.find(" ")} letras")
+
+separa = nome.split()
+print(f"Seu primeiro nome é {separa[0]}, e tem {len(separa[0])} letras")
