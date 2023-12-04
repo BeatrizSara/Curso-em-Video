ef75b3cad6543558d8833e5e1e7a70f9f539ff41
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-03 23:40:52 -0300

    finalizado exercicio manipulando texto 02

--- exemplos_aprendizado/manipulando_texto/02.py
+++ exemplos_aprendizado/manipulando_texto/02.py
@@ -4,3 +4,16 @@ ex:
 Digite um número: 1834 
 unidade: 4 dezena: 3 centena: 8 milhar: 1
 """
+
+num = int(input("Informe um número: "))
+
+u = num// 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print(f"Analisando o número {num}")
+print(f"Unidade: {u}")
+print(f"Dezena: {d}")
+print(f"Centena: {c}")
+print(f"Milhar: {m}")
