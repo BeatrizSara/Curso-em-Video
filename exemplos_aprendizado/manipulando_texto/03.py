2b4a78112db1b15e99f9f99e62ec2fce7c6f849b
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-04 21:38:38 -0300

    Atualizado exercicio 04 para 03 manipulando texto

--- exemplos_aprendizado/manipulando_texto/03.py
+++ exemplos_aprendizado/manipulando_texto/03.py
@@ -1,4 +1,5 @@
 """ 
-Separando os digitos de um número
-Crie um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados.
+Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO"
 """
+cid = str(input("Qual cidade você nasceu? "))
+print(cid[:5].upper() == "SANTO")
