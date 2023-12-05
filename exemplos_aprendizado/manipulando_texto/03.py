9e62a0fc062887e5b66808121180474c6c80eed1
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-04 22:50:56 -0300

    atualizado exercicio 03

--- exemplos_aprendizado/manipulando_texto/03.py
+++ exemplos_aprendizado/manipulando_texto/03.py
@@ -1,4 +1,5 @@
 """ 
+Verificando as primeiras letras dentro de um texto
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO"
 """
 cid = str(input("Qual cidade você nasceu? "))
