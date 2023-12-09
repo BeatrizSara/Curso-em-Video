62ca179e8b837e5ad3ba3367afba3cb0a49432fc
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-08 23:08:27 -0300

    exercicio finalizado estrutura condicional 02

--- exemplos_aprendizado/estrutura_condicional/02.py
+++ exemplos_aprendizado/estrutura_condicional/02.py
@@ -4,3 +4,12 @@ Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa vai custar R$ 7,00 por cada Km acima do limite.
 """
+
+velocidade = float(input("Qual é a velocidade atual do carro? "))
+
+if velocidade > 80:
+    print("Multado! Excedeu o limite permitido de 80Km/h")
+    multa = (velocidade - 80) * 7
+    print(f"Voce deve pagar uma multa no valor de R$ {multa:.2f}")
+
+print("Tenha um ótimo dia.\nDirija com cuidado!")
