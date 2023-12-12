e85fb8afa7863887fefea3e016a7c221581fc9cc
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-11 22:18:08 -0300

    atualizado exercicio estrutura condicional 03

--- exemplos_aprendizado/estrutura_condicional/03.py
+++ exemplos_aprendizado/estrutura_condicional/03.py
@@ -2,6 +2,9 @@
 Par ou Ímpar?
 Crie um programa que leia um número inteiro
 """
+#Obs: Usou o operador de porcentagem para calcular o resto da divisão. 
+#Todo número par é divisivel por 2 ou o resto da divisão por 2 é igual a zero
+
 numero = int(input("Informe um número aleatório inteiro: "))
 
 resultado = numero % 2
