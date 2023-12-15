82dae3b1df293901d337e3afec53e7e0d91056ba
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-15 20:10:55 -0300

    exemplo sistema cores terminal 002

---
+++ exemplos_aprendizado/sistema_cores_terminal/002.py
@@ -0,0 +1,10 @@
+""" 
+# Números com o resultado em cores diferentes
+
+"""
+
+a = 3
+b = 5
+s = a + b
+print(f"Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m")
+print(f"A soma dos valores \033[32m{a}\033[m + \033[31m{b}\033[m são \033[33m{s}\033[m")
