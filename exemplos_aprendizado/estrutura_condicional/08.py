11c0b1d6b6d6dc2174c9844cd827d746503ce6b7
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-15 20:09:41 -0300

    exercicio estrutura condicional 08

--- exemplos_aprendizado/estrutura_condicional/08.py
+++ exemplos_aprendizado/estrutura_condicional/08.py
@@ -1,3 +1,22 @@
 """ 
-Analisando Triângulos
+Analisando Triângulo
+Desenvolva um programa que leia o comprimento de três retas 
+e diga ao usuário se elas podem ou não formar um triângulo.
 """
+
+print("*" * 31)
+print(" ▲  ANALISADOR DE TRIÂNGULOS ▲ ") 
+print("*" * 31)
+
+ladoA = float(input("Digite um valor para formar um triângulo: "))
+ladoB = float(input("Digite um segundo valor para formar um triângulo: "))
+ladoC = float(input("Digite um terceiro valor para formar um triângulo: "))
+
+#Triangle is True if : a + b > c and a + c > b and b + c > a
+
+
+if ladoA + ladoB > ladoC  and ladoA + ladoC > ladoB and ladoB + ladoC > ladoA:
+    print("Os valores acima PODEM FORMAR UM TRIÂNGULO!")
+
+else:
+    print("Os valores acima NÃO PODEM FORMAR UM TRIÂNGULO!") 
