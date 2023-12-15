fbc1ce532fcba025b78edb1a92e686a3dc4f54de
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-15 20:11:30 -0300

    editado exemplo sistema cores terminal 003

--- exemplos_aprendizado/sistema_cores_terminal/003.py
+++ exemplos_aprendizado/sistema_cores_terminal/003.py
@@ -7,4 +7,3 @@ print(f"😊 Olá! Muito prazer em te conhecer, \033[4;35;40m{nome}\033[m \u2764
 
 #ou utilizar outra forma para exibir a mensagem
 print("😊 Olá! Muito prazer em te conhecer, {}{}{}".format("\033[7;45m", nome,"\033[m \u2764"))
-
