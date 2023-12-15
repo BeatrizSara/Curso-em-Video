0990a67746c9688778a4c3a8448d0c8f74bcf37f
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-15 20:10:40 -0300

    exemplo sistema cores terminal 001

---
+++ exemplos_aprendizado/sistema_cores_terminal/001.py
@@ -0,0 +1,20 @@
+""" 
+Sistema de Cores no Terminal
+STYLE \ TEXT \ BACK
+ 0       30     40 - branco
+ 1       31     41 - vermelho
+ 4       32     42 - verde
+ 7       33     43 - amarelo
+         34     44 - azul
+         35     45 - roxo
+         36     46 - ciano
+         37     47 - cinza(padrão)
+"""
+
+print("\033[1;31;43mOlá, Mundo!\033[m")
+print("\033[4;37;45mOlá, Mundo!\033[m")
+print("\033[0;33;44mOlá, Mundo!\033[m")
+print("\033[7;37mOlá, Mundo!\033[m")
+print("\033[7;30;41mOlá, Mundo!\033[m")
+print("\033[0;30;41mOlá, Mundo!\033[m")
+
