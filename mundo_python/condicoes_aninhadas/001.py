0a5464530d46d89cf898e43cc065665778af6d4f
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-18 22:37:56 -0300

    iniciando exercicio condicoes aninhadas 001

--- mundo_python/condicoes_aninhadas/001.py
+++ mundo_python/condicoes_aninhadas/001.py
@@ -1,4 +1,6 @@
 """ 
 Aprovando Emprestimos
-
+Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. 
+Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+A prestação mensal, não pode exceder 30% do salário ou entao o emprestimo será negado.
 """
