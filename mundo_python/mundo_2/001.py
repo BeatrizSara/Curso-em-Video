cd5af2a3a5ee49073ce6c82dff2b8145fc7070ca
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-20 18:07:27 -0300

    finalizado exercicio condicoes aninhadas 001

---
+++ mundo_python/mundo_2/001.py
@@ -0,0 +1,23 @@
+""" 
+Aprovando Emprestimo
+Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. 
+Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+A prestação mensal, não pode exceder 30% do salário ou entao o emprestimo será negado.
+"""
+
+valor_casa = float(input("Qual o valor desejado da casa? R$ "))
+salario = float(input("Qual o salario do comprador? R$"))
+anos = int(input("Quantos anos de financiamento? "))
+
+prestacao_mensal = valor_casa / (anos * 12) 
+limite = salario * 0.3 
+
+#O valor da prestação mensal do empréstimo não deve ser maior do que 30% do salário do comprador. Se não, será negado!
+if prestacao_mensal <= limite:
+    print("Empréstimo APROVADO!")
+    print(f"Para pagar uma casa de R${valor_casa:.2f} \nA prestação será de R$ {prestacao_mensal:.2f}")
+ 
+    
+else:
+    print("Empréstimo NEGADO!")
+    
