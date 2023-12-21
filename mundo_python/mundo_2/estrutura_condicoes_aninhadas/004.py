72bd356f0e9578790c0bb547c7f7f642fd0d16d7
Beatriz Sara <bea.sara.santos@outlook.com>
2023-12-21 19:59:31 -0300

    iniciando exercicio condicoes aninhadas 004

---
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/004.py
@@ -0,0 +1,8 @@
+""" 
+Alistamento Militar
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade, 
+se ele ainda vai se alistar ao serviço militar,
+se já passou do tempo do alistamento. 
+Seu programa também deverá mostrar :
+o tempo que FALTA ou que PASSOU do prazo
+"""
