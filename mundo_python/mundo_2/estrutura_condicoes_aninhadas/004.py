d5698245eb3fb6dcc95e9646f8961c6a5c8204ee
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-03 19:31:57 -0300

    alterado exercicio 004

--- mundo_python/mundo_2/estrutura_condicoes_aninhadas/004.py
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/004.py
@@ -5,25 +5,31 @@ se ele ainda vai se alistar ao serviço militar,
 se já passou do tempo do alistamento. 
 Seu programa também deverá mostrar :
 o tempo que FALTA ou que PASSOU do prazo
+O programa deve identificar se é Homem ou mulher - Masculino ou Feminino
 """
 from datetime import date
 
 atual = date.today().year
 nascimento = int(input("Qual o ano de nascimento: "))
+sexo = str(input("Feminino - F ou Masculino - M: ")).upper()
 idade = atual - nascimento
+
 print("Nascimento no ano {} tem {} anos".format(nascimento,idade,atual))
-
-if idade == 18:
-    print("Já possui a idade obrigatória para o alistamento")
-
-elif idade < 18:
-    saldo = 18 - idade 
-    ano_alistamento = atual + saldo
-    print(f"Ainda não possui a idade para se alistar. Falta {saldo} anos")
-    print(f"Seu alistamento será em {ano_alistamento}")
+if sexo == "F":
+    print("Você não precisa realizar o alistamento militar")
 
 else:
-    saldo = idade - 18
-    ano_alistamento = atual - saldo
-    print(f"Sua idade já passou da idade obrigatória para se alistar. Deveria ter se alistado a {saldo} anos")
-    print(f"Seu alistamento foi em {ano_alistamento}")
+    if idade == 18:
+        print("Já possui a idade obrigatória para o alistamento")
+
+    elif idade < 18:
+        saldo = 18 - idade 
+        ano_alistamento = atual + saldo
+        print(f"Ainda não possui a idade para se alistar. Falta {saldo} anos")
+        print(f"Seu alistamento será em {ano_alistamento}")
+
+    else:
+        saldo = idade - 18
+        ano_alistamento = atual - saldo
+        print(f"Sua idade já passou da idade obrigatória para se alistar. Deveria ter se alistado a {saldo} anos")
+        print(f"Seu alistamento foi em {ano_alistamento}")
