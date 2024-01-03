7885e3065033aaa0c966f520ee1501aab571ea7f
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-02 22:37:03 -0300

    realizando exercicio 004

--- mundo_python/mundo_2/estrutura_condicoes_aninhadas/004.py
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/004.py
@@ -6,4 +6,15 @@ se já passou do tempo do alistamento.
 Seu programa também deverá mostrar :
 o tempo que FALTA ou que PASSOU do prazo
 """
+from datetime import date
+
+atual = date.today().year
 ano = int(input("Qual o ano de nascimento: "))
+idade = atual - ano
+print("Nascimento no ano {} tem {} anos").format(ano,idade,atual)
+
+if idade == 18:
+    print("Já possui a idade obrigatória para o alistamento")
+
+elif idade < 18:
+    print("Ainda não possui a idade para se alistar")
