db16efae8e96fce1fd385a7b3ff423304b61ef9d
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-08 22:26:05 -0300

    007

---
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/007.py
@@ -0,0 +1,12 @@
+""" 
+Analisando triangulos
+Refaça o desafio 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+OBS: mundo 1 - estrutura condicional 08
+- Equilátero: todos os lados iguais
+- Isósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+"""
+print("Informe a seguir, números para formar um tipo de triângulo")
+lado_a = float(input("\nInforme o primeiro número: "))
+lado_b = float(input("Informe o segundo número: "))
+lado_c = float(input("Informe o terceiro número: "))
