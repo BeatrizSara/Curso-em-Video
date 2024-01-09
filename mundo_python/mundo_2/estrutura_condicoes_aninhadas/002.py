5c69038fa7d7a346d850f615dcad0b5bb8eb634b
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-09 17:55:49 -0300

    002 para 037

--- mundo_python/mundo_2/estrutura_condicoes_aninhadas/002.py
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/002.py
@@ -1,5 +1,5 @@
 """ 
-Conversor de Bases Numéricas  - Sistema de Numeração
+Conversor de Bases Numéricas  - Sistema de Numeração - 037
 Escreva um programa que leia um número inteiro qualquer e
 peça para o usuário  escolher qual será a base de conversão:
 - 1 para binário = 0b
