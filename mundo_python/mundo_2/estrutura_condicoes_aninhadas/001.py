805b55860b57de8009ffb4dbeca47c8a4b1cec80
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-09 17:55:32 -0300

    001 para 036

--- mundo_python/mundo_2/estrutura_condicoes_aninhadas/001.py
+++ mundo_python/mundo_2/estrutura_condicoes_aninhadas/001.py
@@ -1,5 +1,5 @@
 """ 
-Aprovando Emprestimo
+Aprovando Emprestimo - 036
 Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. 
 Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 A prestação mensal, não pode exceder 30% do salário ou entao o emprestimo será negado.
