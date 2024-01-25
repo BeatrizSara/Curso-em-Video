d599fd0da62bf048ff8ff6f3d8b66f8ea3ef9617
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-24 23:00:48 -0300

    exeercicio estrutura repeticao 001

--- mundo_python/mundo_2/estrutura_repeticao_for/001.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/001.py
@@ -1,4 +1,18 @@
 """ 
-Estrutura de Repetição FOR
-
+Exercicio 46
+Faça um programa que msotre na tela uma contagem regressiva para o estouro de fogos de artificio,
+indo de 10 até 0, com uma pausa de 1 ou 0.5 segundo entre eles
+meio segundo - sleep(0.5)
 """
+from time import sleep
+
+print("INICIANDO")
+sleep(1)
+print("CONTAGEM REGRESSIVA")
+sleep(1)
+
+for contagem in range(10, -1, -1):
+    sleep(0.5)
+    print(contagem)
+sleep(1)
+print("🎇  🎇  🎇 HAPPY NEW YEAR  🎇  🎇  🎇 ")
