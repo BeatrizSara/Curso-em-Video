d963748dba1ca6003de0047388fea6444a20a96a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-01-24 22:54:27 -0300

    exemplo estrutura rep for 03

---
+++ mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_03.py
@@ -0,0 +1,9 @@
+
+# Realizar um somatorio desses números
+# s += n é a mesma coisa que s = s + n
+
+s = 0
+for c in range(0, 4):
+    n = int(input("Digite um valor: "))
+    s += n
+print("O somatório de todos os valores foi {}".format(s))
