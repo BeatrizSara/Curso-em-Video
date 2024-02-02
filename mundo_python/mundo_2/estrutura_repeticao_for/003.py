7d06dff6a6d23ef84be11da8d72017e3bdfde203
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-01 21:58:41 -0300

    003

--- mundo_python/mundo_2/estrutura_repeticao_for/003.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/003.py
@@ -1,4 +1,6 @@
 """ 
+Exercicio 048
+Soma ímpares múltiplos de 3
 Faça um programa que calcule a soma entre todos os numeros impares ( nao é para somar todos), mas sim,
 que são múltiplos de três e que 
 se encontram no intervalo de 1 até 500.
