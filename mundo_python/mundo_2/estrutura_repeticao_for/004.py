14d57bd4177e498aba3b26e32b542795a3bf1bda
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-02 19:20:35 -0300

    004

--- mundo_python/mundo_2/estrutura_repeticao_for/004.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/004.py
@@ -2,5 +2,5 @@
 Exercicio 049
 Tabuadav.2.0
 Refaça o Desafio 009, mostrando a tabuada de um número que o usuário escolhe, 
-só que agora utilizando um laço for. 
+só que agora utilizando um laço for 
 """
