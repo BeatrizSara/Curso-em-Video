4718ac4ee4f072babb22edf65b40bd97275269dd
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-07 21:36:07 -0300

    exercicio repeticao for 005

--- mundo_python/mundo_2/estrutura_repeticao_for/005.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/005.py
@@ -1,8 +1,16 @@
 """ 
 Exercicio 050
-
+Desenvolva um programa que leia seis numeros inteiros 
+e mostre a soma apenas daqueles que forem pares.
+Se o valor digitado for impar, desconsidere-o.
 """
+soma = 0 
+contador = 0
 
-
-for c in range(1, 7):
-    numeros = int(input("Digite um valor: ".format(c)))
+for par in range(1, 7):
+        num = int(input("Informe o {} valor: ".format(par)))
+        if num % 2 == 0:
+            soma += num
+            contador += 1
+        
+print("Foi informado {} numeros pares, e a soma foi: {}".format(contador, soma))
