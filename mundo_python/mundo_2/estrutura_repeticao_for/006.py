7b5ab92eb624ccf766423935584add2758809941
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-12 18:43:45 -0300

    finalizado exercicio repeticao for 051

--- mundo_python/mundo_2/estrutura_repeticao_for/006.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/006.py
@@ -1,4 +1,16 @@
 """ 
 Exercicio 051
-Gerenciador de Pagamentos 
+Progressao Aritimética
+Desenvolva um programa que leia o primeiro termo e a razao de uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.
+an = an-1 + r
+formula do n - ésimo termo 
 """ 
+primeiro_termo = int(input("Primeiro Termo: "))
+razao = int(input("Razao entre eles: "))
+decimo_termo = primeiro_termo + (10 -1) * razao
+
+for c in range(primeiro_termo, decimo_termo + razao, razao):
+    print("{}".format(c), end = " => ")
+    
+print("fim")
