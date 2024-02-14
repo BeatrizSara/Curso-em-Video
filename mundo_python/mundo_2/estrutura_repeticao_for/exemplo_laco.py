eb15f17b1cc6a4d67576b786cbfba70672366a63
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-13 21:35:55 -0300

    Finalizado exemplo strutura de laco e cores no terminal

--- mundo_python/mundo_2/estrutura_repeticao_for/exemplo_laco.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_laco.py
@@ -7,3 +7,23 @@ while True:
  ##  ##   ##   #  ##  ##     ##      ##  ##    ##     ##    #
 ######   #######  ##  ##    ####    #### ##   ####    #######""")
     break
+
+import random
+from colorama import init, Fore, Style
+
+# Inicializa o colorama (apenas necessário no Windows)
+init()
+
+while True:
+    # Gera uma cor RGB aleatória
+    def cor_aleatoria():
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        return f'\033[38;2;{r};{g};{b}m'
+
+    # Exemplo de uso com cor aleatória
+    texto = 'BEATRIZ!'
+    cor = cor_aleatoria()
+
+    print(cor + texto + Style.RESET_ALL)
