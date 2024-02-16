c7956407da06ca39a0aa65f83abe5860d07f11d2
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-16 19:47:11 -0300

    editado 003

--- mundo_python/mundo_2/estrutura_repeticao_for/003.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/003.py
@@ -4,7 +4,7 @@ Soma ímpares múltiplos de 3
 Faça um programa que calcule a soma entre todos os numeros impares ( nao é para somar todos), mas sim,
 que são múltiplos de três e que 
 se encontram no intervalo de 1 até 500.
-(Soma dos numeros
+(Soma dos numeros)
 Contador de quantos numeros tem dentro de 1 a 500 que sao multiplos de 3)
 """
 
@@ -17,6 +17,6 @@ for numero in range(1,501,2):
         numeros_impares.append(numero) 
         contador +=  1 #contador - soma com + 1
         soma += numero #acumulador - soma com o valor
-        
+
 print(f"A soma de todos os {contador} valores solicitados é: {soma}")
 print("Os numeros dentro de 1 a 500 que são multiplos de 3 são:\n{}".format(numeros_impares))
