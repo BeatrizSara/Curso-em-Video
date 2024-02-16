a904f3489724b64c88d4d391244df62bc6f83c58
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-16 19:47:46 -0300

    editado 007

--- mundo_python/mundo_2/estrutura_repeticao_for/007.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/007.py
@@ -1,9 +1,9 @@
 """ 
 Exercicio 052
 Números primos
-Faça um programa que leia um numero inteiro e diga 
-se ele é ou nao um número primo. (só é divisivel por 1 ou por ele mesmo)
+Faça um programa que leia um numero inteiro e diga se ele é ou nao um número primo. 
+(só é divisivel por 1 ou por ele mesmo)
 """
 num = int(input("Número inteiro:"))
 # if num > 1
-for c in range
+for c in range()
