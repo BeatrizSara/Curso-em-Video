9d3990d3c90ff81df0f4333558a06be26da332ed
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-15 22:29:02 -0300

    007

--- mundo_python/mundo_2/estrutura_repeticao_for/007.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/007.py
@@ -5,6 +5,5 @@ Faça um programa que leia um numero inteiro e diga
 se ele é ou nao um número primo. (só é divisivel por 1 ou por ele mesmo)
 """
 num = int(input("Número inteiro:"))
-
-for c in range
 # if num > 1
+for c in range
