e11e8a6133fc6bd4617deb2144d7a216c904f4a2
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-16 19:46:43 -0300

    editado 002

--- mundo_python/mundo_2/estrutura_repeticao_for/002.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/002.py
@@ -3,6 +3,7 @@ Exercicio 047
 Contagem de Pares
 Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 """
+
 numeros_pares = []
 
 for numero in range(1,51):
