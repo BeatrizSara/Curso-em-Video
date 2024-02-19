b4582f6249148fd9027725973aa12c13c546d945
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-18 21:13:12 -0300

    001 editado

--- mundo_python/mundo_2/estrutura_repeticao_for/007.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/007.py
@@ -5,5 +5,6 @@ Faça um programa que leia um numero inteiro e diga se ele é ou nao um número 
 (só é divisivel por 1 ou por ele mesmo).
 """
 num = int(input("Número inteiro:"))
+
 # if num > 1
 #for c in range()
