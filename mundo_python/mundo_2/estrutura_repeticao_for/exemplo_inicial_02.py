ba6fa7a7dd2d4b07b4d265c1db6ce2e472e841a3
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-19 21:38:48 -0300

    exemplo simples estrutura rep for 02

---
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_02.py
@@ -0,0 +1,8 @@
+"""Exemplos simples de Estrutura de repetição FOR"""
+
+# -1 informa qual é a ITERAÇÃO - Que vai acontecer no final do laço
+for c in range(6, 0, -1): # Se nao tiver o -1, o programa só vai exibir a contagem regressiva.
+    print(c)
+print("FIM")
+
+# esta contando o numero 6 - 1 : 5 - 1: 4 -1: 3 -1: 2 -1 : 1 ( Nao vai aparecer o 0 por ser o último número)
