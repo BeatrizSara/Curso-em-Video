0be5492978f675ee319045432307d94106e6bce6
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-19 21:28:52 -0300

    exemplo simples  estrutura rep for 01

---
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_01.py
@@ -0,0 +1,17 @@
+"""Exemplos simples de Estrutura de repetição FOR"""
+
+#EXEMPLO 01
+for c in range(1, 6):
+    print("Oi")
+print("FIM")
+
+#EXEMPLO 02
+for c in range(0, 6):
+    print("Oi")
+print("FIM")
+
+#EXEMPLO 03
+for c in range(1, 7):
+    print(c)
+print("FIM")
+
