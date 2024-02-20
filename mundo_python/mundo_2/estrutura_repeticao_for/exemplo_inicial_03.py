b20801508f7907eb86cf9beef348d11dc7453c2e
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-19 21:47:44 -0300

    exemplo simples  estrutura for 03

---
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_03.py
@@ -0,0 +1,20 @@
+"""Exemplos simples de Estrutura de repetição FOR"""
+
+# A contagem é de 2 em 2 - chamada ITERAÇÃO ; Contou de 0 ate 7 de 2 em 2.
+for c in range (0, 7,  2): 
+    print(c)
+print("FIM")
+
+# Outro exemplo 
+n  = int(input("Informe um número: "))
+         
+for c in range(0, n):
+    print(c)
+print("fim")
+
+# Outro exemplo 
+n = int(input("Informe um número: "))
+
+for c in range(0, n+1 ):
+    print(c)
+print("FIM")
