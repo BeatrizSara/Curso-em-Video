f7f42ca5ede58b9777c77ac231e403e9453382dc
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-22 21:55:58 -0300

    ediatdo exemplo str rep 01

--- mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_01.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_01.py
@@ -18,6 +18,7 @@ FIM
 # Passo significa de quanto em quanto o numero informado vai pular.
 # Se for: inicio 1, fim 100 e passo 10, vai contar de  10 em 10 - 1 11 21 31 41 51.. 100
 #Pode-se utilizar da estrutura de repetição para fazer o que voce quiser
+
 i = int(input("Início: "))
 f = int(input("Fim: "))
 p = int(input("Passo: "))
