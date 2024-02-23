43d7207f8b221bffb0b695601bbdb4a09aca2964
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-22 21:56:13 -0300

    ediatdo exemplo str rep 02

--- mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_02.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_02.py
@@ -1,5 +1,6 @@
 # Vai pedir os valores de acordo com o valor no range. Se for 3 vai pedir 3x para digitar um valor
 #tipo de situação importante
+
 for c in range(0, 10):
     n = int(input("Digite um valor: "))
 print("FIM")
