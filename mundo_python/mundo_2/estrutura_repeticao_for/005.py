90a9d964f08bb17f580ae5d23873460c059ebbaa
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-24 23:28:30 -0300

    editado exercicio  005 - 050

--- mundo_python/mundo_2/estrutura_repeticao_for/005.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/005.py
@@ -1,5 +1,6 @@
 """ 
-Exercicio 050 - Soma dos pares
+Exercicio 050
+Soma dos pares
 Desenvolva um programa que leia seis numeros inteiros 
 e mostre a soma apenas daqueles que forem pares.
 Se o valor digitado for impar, desconsidere-o.
