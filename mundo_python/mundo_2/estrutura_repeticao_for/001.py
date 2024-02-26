170de7f8598ed5b2248197338cfae315e5c169b0
Beatriz Sara <bea.sara.santos@outlook.com>
2024-02-26 20:36:49 -0300

    editado exercicio 001

--- mundo_python/mundo_2/estrutura_repeticao_for/001.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/001.py
@@ -4,7 +4,6 @@ Contagem Regressiva
 Faça um programa que msotre na tela uma contagem regressiva para o estouro de fogos de artificio,
 indo de 10 até 0, com uma pausa de 1 ou 0.5 segundo entre eles. "Meio segundo - sleep(0.5)"
 """
-
 from time import sleep
 
 print("INICIANDO")
