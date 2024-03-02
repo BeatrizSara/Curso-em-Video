c2442ca2f5cb0799aa823fc1c76681460a9d59b0
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-01 21:21:45 -0300

    editado exemplo for 02

--- mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_02.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_02.py
@@ -1,7 +1,8 @@
 """Exemplos simples de Estrutura de repetição FOR"""
 
 # -1 informa qual é a ITERAÇÃO - Que vai acontecer no final do laço
-# Se nao tiver o -1, o programa só vai exibir a contagem regressiva.
+# Se nao tiver o -1, o programa só vai exibir a contagem regressiva. 
+# -1 é ITERAÇÃO
 
 for c in range(6, 0, -1): 
     print(c)
