a930fab1d65922e61b1bd4d487e32bdf12603d29
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-01 21:22:08 -0300

    exemplo inicial 04

--- mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_04.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_04.py
@@ -1,1 +1,15 @@
 """Exemplos simples de Estrutura de repetição FOR"""
+n = int(input("Digite um número: "))
+for c in range(0,n):
+    print(c)
+print("FIM")
+
+#Utilizo o "n" como parte de passagem pro FOR
+# Posso usar essa mesma linha de raciocinio
+
+inicio = int(input("Inicio: "))
+fim = int(input("Fim: "))
+passo = int(input("Passo: "))
+for c in range( inicio, fim+1, passo):
+    print(c)
+print("FIM")
