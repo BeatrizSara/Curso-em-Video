b401c021c8b67d70b3c7589d4bee2d08db36872e
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-03 22:57:01 -0300

    editado

--- mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_02.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/exemplo_inicial_02.py
@@ -3,7 +3,7 @@
 # -1 informa qual é a ITERAÇÃO - Que vai acontecer no final do laço.
 # Se nao tiver o -1, o programa só vai exibir a contagem regressiva. 
 # -1 é ITERAÇÃO
-
+ 
 for c in range(6, 0, -1): 
     print(c)
 print("FIM")
