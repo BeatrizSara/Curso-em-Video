1c9b1e2b10eeccebf84c20604736d3f43f7853b6
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-05 21:55:07 -0300

    exemplo inicial repticao for 05

---
+++ mundo_python/mundo_2/estrutura_repeticao_for/exem_estr_rep_for_inicial_05.py
@@ -0,0 +1,10 @@
+# Exemplo simples
+for c in range(0, 3):
+    n = int(input("Informe um número: "))
+print("Fim")
+
+# posso pedir para perguntar a mesma coisa mais de 3x. Pode ser 5x por exemplo
+
+for c in  range(0, 5):
+    n = int(input("Informe um número: "))
+print("FIM")
