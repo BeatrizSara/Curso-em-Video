d8ca510534ea3d67620fc85a5e67e891c2ef9e3d
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-16 23:24:55 -0300

    Editado exercicio 004

--- mundo_python/mundo_2/estrutura_repeticao_for/004.py
+++ mundo_python/mundo_2/estrutura_repeticao_for/004.py
@@ -1,8 +1,7 @@
 """ 
 Exercicio 049
 Tabuada.2.0
-Refaça o Desafio 009, mostrando a tabuada de um número que 
-o usuário escolhe, só que agora utilizando um laço for 
+Refaça o Desafio 009, mostrando a tabuada de um número que o usuário escolhe, só que agora utilizando um laço for 
 """
 numero = int(input("Informe um número para verificar a TABUADA : "))
 
